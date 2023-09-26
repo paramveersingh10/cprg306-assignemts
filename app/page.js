e6aa5988150c87e1956bc391e019469aa6b8978a
paramveersingh10 <120078017+paramveersingh10@users.noreply.github.com>
2023-09-25 21:48:46 -0800

    done

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
-import link from 'next/link';
+import Link from 'next/link';
 import StudentInfo from './StudentInfo';
 export default function Page()
     {
         return (
             <div>
                 <h1>CPRG 306: Web Development 2 - Assigments </h1>
-                <StudentInfo />
-                <link href="\week2">week2</link>
+                <StudentInfo/>
+                <Link href="\week2">week2</Link>
             </div>  
         )
     }     
---
+++ app/week2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from './StudentInfo.js';
+export default function Page()
+    {
+        return (
+            <div>
+                <h1> My Shopping List </h1>
+                <StudentInfo />
+            </div>  
+        )
+    }
