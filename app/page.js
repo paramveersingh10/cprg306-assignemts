8b81d9bd3af955a65a13481429b3d329ea3cd5da
paramveersingh10 <120078017+paramveersingh10@users.noreply.github.com>
2023-10-02 20:03:09 -0800

    week_3

--- app/page.js
+++ app/page.js
@@ -7,6 +7,8 @@ export default function Page()
                 <h1>CPRG 306: Web Development 2 - Assigments </h1>
                 <StudentInfo/>
                 <Link href="\week2">week2</Link>
+                <p></p>
+                <Link href="\week3">week3</Link>
             </div>  
         )
     }     
---
+++ app/week3/page.js
@@ -0,0 +1,15 @@
+// page.js
+
+import React from 'react';
+import ItemList from './item-list'; // Import the ItemList component
+
+const Page = () => {
+  return (
+    <main className="bg-gray-200 min-h-screen p-6">
+      <h1 className="text-3xl font-semibold mb-4">Shopping List</h1>
+      <ItemList />
+    </main>
+  );
+};
+
+export default Page;
